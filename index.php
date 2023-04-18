3bff1259b7dc471d65c743290cd4827ada9edb7e
edsonhashimoto <edson.hh@gmail.com>
2023-04-17 21:58:58 -0300

    arquivos

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>VGCJ GROUP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!--
+    <header>
+        
+        <nav>
+            <a href="http://">HOME</a>
+            <a href="http://">ABOUT VGCJ</a>
+            <a href="http://">PRODUCTS</a>
+            <a href="http://">SERVICES</a>
+            <a href="http://">CONTACT</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    -->
+    
+    <div class="container-fluid" >
+        <div class="container">
+            <div class="row">
+                <div class="col-4 text-center" >
+                    <img src="img/logo-ancora-vgcj-group-vertical.png" height="60">
+                </div>
+                <div class="col-8" >
+                    <div class="row">
+                        <div class="col-2 text-center" style="margin-top:24px; margin-bottom: 24px;">
+                            <span style="font-size: large;"><strong>HOME</strong></span>
+                        </div>
+                        <div class="col-2 text-center" style="margin-top:24px; margin-bottom: 24px;">
+                            <span style="font-size: large;"><strong>VGCJ</strong></span>
+                        </div>
+                        <div class="col-2 text-center" style="margin-top:24px; margin-bottom: 24px;">
+                            <span style="font-size: large;"><strong>PRODUCTS</strong></span>
+                        </div>
+                        <div class="col-2 text-center" style="margin-top:24px; margin-bottom: 24px;">
+                            <span style="font-size: large;"><strong>SERVICES</strong></span>
+                        </div>
+                        <div class="col-2 text-center" style="margin-top:24px; margin-bottom: 24px;">
+                            <span style="font-size: large;"><strong>CONTACT</strong></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ index_3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <title>VGCJ GROUP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    
+    <div class="container-fluid" style="background-color: red;">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 text-center" style="background-color: green;">
+                    <img src="/imgDefault/logo.png" height="60" style="margin: 10px;">
+                </div>
+                <div class="col-8" style="background-color: green;">
+                    <div class="row">
+                        <div class="col" style="background-color: green; margin:auto">
+                            <span style="font-size: large;"><strong>HOME</strong></span>
+                            <span style="font-size: large;"><strong>VGCJ</strong></span>
+                            <span style="font-size: large;"><strong>PRODUCTS</strong></span>
+                            <span style="font-size: large;"><strong>SERVICES</strong></span>
+                            <span style="font-size: large;"><strong>CONTACT</strong></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. A praesentium veritatis quidem! Eos aperiam aspernatur sed quaerat, libero voluptatum ut rem sunt minus voluptate tenetur eveniet ipsa nulla pariatur quibusdam!
+    locale_filter_matches
+    Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Exercitationem, at! Asperiores deleniti, eum impedit nobis voluptatibus quidem vero ipsam neque est quaerat, quo minus sint aliquid harum earum iusto ipsum!
+    
+
+    aaaaa
+    bbbbb
+    ccccc
+    
+</body>
+
+</html>
