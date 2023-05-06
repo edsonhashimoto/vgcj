4abfca0cf771aa3ad0fceb5cf89c521b0f40a776
edsonhashimoto <edson.hh@gmail.com>
2023-05-06 16:56:26 -0300

    tradução

--- headerFooter.js
+++ headerFooter.js
@@ -1,70 +1,65 @@
-$(document).ready(
-    async function () {
+export async function HeaderFotter(funcaoExtra) {
+    var response = await fetch('./header.html');
+    var dados = await response.text();
+    var div = document.getElementById("header");
+    div.innerHTML = dados;
 
-        var response = await fetch('./header.html');
-        var dados = await response.text();
-        var div = document.getElementById("header");
-        div.innerHTML = dados;
+    var response = await fetch('./footer.html');
+    var dados = await response.text();
+    var div = document.getElementById("footer");
+    div.innerHTML = dados;
 
-        var response = await fetch('./footer.html');
-        var dados = await response.text();
-        var div = document.getElementById("footer");
-        div.innerHTML = dados;
-
-        async function loadLanguage() {
-
-            var response = await fetch('language.json');
-            var dados = await response.json();
-            var language;
-            if (!localStorage.getItem('lang') || localStorage.getItem('lang') == 'us') {
-                language = dados.us;
-            }
-            else if (localStorage.getItem('lang') == 'br') {
-                language = dados.br;
-            }
-            else if (localStorage.getItem('lang') == 'tw') {
-                language = dados.tw;
-            }
-
-            document.getElementById("mHome").innerHTML = language.mHome;
-            document.getElementById("mAbout").innerHTML = language.mAbout;
-            document.getElementById("mProducts").innerHTML = language.mProducts;
-            document.getElementById("mServices").innerHTML = language.mServices;
-            document.getElementById("mContact").innerHTML = language.mContact;
-
-            //document.getElementById("mProductLine").innerHTML = language.mProductLine;
-            document.getElementById("mAssemblyLine").innerHTML = language.mAssemblyLine;
-            document.getElementById("mPlateMaking").innerHTML = language.mPlateMaking;
-            document.getElementById("mFinishLine").innerHTML = language.mFinishLine;
-            document.getElementById("mCompleteProject").innerHTML = language.mCompleteProject;
-            document.getElementById("mUniqueEquipment").innerHTML = language.mUniqueEquipment;
-            document.getElementById("mMaterialSupply").innerHTML = language.mMaterialSupply;
-
+    async function loadLanguage() {
+        var response = await fetch('language.json');
+        var dados = await response.json();
+        var language;
+        if (!localStorage.getItem('lang') || localStorage.getItem('lang') == 'us') {
+            language = dados.us;
         }
+        else if (localStorage.getItem('lang') == 'br') {
+            language = dados.br;
+        }
+        else if (localStorage.getItem('lang') == 'tw') {
+            language = dados.tw;
+        }
+        localStorage.setItem('language', JSON.stringify(language));
 
-        loadLanguage();
+        document.getElementById("mHome").innerHTML = language.Home.toUpperCase();;
+        document.getElementById("mAbout").innerHTML = language.About.toUpperCase();;
+        document.getElementById("mProducts").innerHTML = language.Products.toUpperCase();;
+        document.getElementById("mServices").innerHTML = language.Services.toUpperCase();;
+        document.getElementById("mContact").innerHTML = language.Contact.toUpperCase();;
 
+        document.getElementById("mAssemblyLine").innerHTML = language.AssemblyLine;
+        document.getElementById("mPlateMaking").innerHTML = language.PlateMaking;
+        document.getElementById("mFinishLine").innerHTML = language.FinishLine;
+        document.getElementById("mCompleteProject").innerHTML = language.CompleteProject;
+        document.getElementById("mUniqueEquipment").innerHTML = language.UniqueEquipment;
+        document.getElementById("mMaterialSupply").innerHTML = language.MaterialSupply;
 
-        $('#flagUsa').on("click",
-            function () {
-                localStorage.setItem('lang', 'us');
-                loadLanguage();
-            }
-        );
-        $('#flagBrazil').on("click",
-            function () {
-                localStorage.setItem('lang', 'br');
-                loadLanguage();
-            }
-        );
+        funcaoExtra();
 
-        $('#flagTaiwan').on("click",
-            function () {
-                localStorage.setItem('lang', 'tw');
-                loadLanguage();
-            }
-        );
     }
 
+    loadLanguage();
 
-);
+    $('#flagUsa').on("click",
+        function () {
+            localStorage.setItem('lang', 'us');
+            loadLanguage();
+        }
+    );
+    $('#flagBrazil').on("click",
+        function () {
+            localStorage.setItem('lang', 'br');
+            loadLanguage();
+        }
+    );
+
+    $('#flagTaiwan').on("click",
+        function () {
+            localStorage.setItem('lang', 'tw');
+            loadLanguage();
+        }
+    );
+}
--- index.js
+++ index.js
@@ -1,109 +1,19 @@
+import { HeaderFotter } from "./headerFooter.js";
+
 $(document).ready(
-
     function () {
-        $('#flagUsa').on("click",
-            function () {
-                localStorage.setItem('lang', 'us');
-                /*
-                $('#mHome').text('HOME');
-                $('#mAbout').text('ABOUT');
-                $('#mProducts').text('PRODUCTS');
-                $('#mServices').text('SERVICES');
-                $('#mContact').text('CONTACT');
-                */
-                /* $('#mProductLine').text('Product Line');
-                $('#mAssemblyLine').text('Assembly Line');
-                $('#mPlateMaking').text('Plate Making');
-                $('#mFinishLine').text('Finishing Line');
-                $('#mCompleteProject').text('Complete Project');
-                $('#mUniqueEquipment').text('Unique Equipment');
-                $('#mMaterialSupply').text('Material Supply'); */
-
-                $('#tProductLine').text('PRODUCT LINE');
-                $('#tAssemblyLine').text('ASSEMBLY LINE');
-                $('#tPlateMaking').text('PLATE MAKING');
-                $('#tFinishLine').text('FINISHING LINE');
-                $('#tCompleteProject').text('COMPLETE PROJECT');
-                $('#tUniqueEquipment').text('UNIQUE EQUIPMENT');
-                $('#tMaterialSupply').text('MATERIAL SUPPLY');
-
-                $('#tWelcome').text('Welcome To VGCJ');
-                $('#tWelcomeText').text('VGCJ\'s focus is on storage power planning and collaborating with energy experts with the goal to cut costs, save energy and reduce carbon emissions. In addition to our extensive experience in storage battery equipment we also provide superior and efficient mechanical equipment and raw materials for lithium-ion and lead acid battery manufacturers that can reduce labor and raise their production capacity. We look forward to being business partners!');
-
-            }
-        );
-        $('#flagBrazil').on("click",
-            function () {
-                localStorage.setItem('lang', 'br');
-
-                /* $('#mHome').text('INÍCIO');
-                $('#mAbout').text('SOBRE');
-                $('#mProducts').text('PRODUTOS');
-                $('#mServices').text('SERVIÇOS');
-                $('#mContact').text('CONTATO'); */
-
-                /* $('#mProductLine').text('Linha De Produtos');
-                $('#mAssemblyLine').text('Linha De Montagem');
-                $('#mPlateMaking').text('Fabricação De Chapas');
-                $('#mFinishLine').text('Linha Final');
-                $('#mCompleteProject').text('Projeto Completo');
-                $('#mUniqueEquipment').text('Equipamento Exclusivo');
-                $('#mMaterialSupply').text('Fornecimento De Materiais'); */
-
-                $('#tProductLine').text('LINHA DE PRODUTOS');
-                $('#tAssemblyLine').text('LINHA DE MONTAGEM');
-                $('#tPlateMaking').text('FABRICAÇÃO DE CHAPAS');
-                $('#tFinishLine').text('LINHA FINAL');
-                $('#tCompleteProject').text('PROJETO COMPLETO');
-                $('#tUniqueEquipment').text('EQUIPAMENTO EXCLUSIVO');
-                $('#tMaterialSupply').text('FORNECIMENTO DE MATERIAIS');
-
-                $('#tWelcome').text('Bem-vindo ao VGCJ');
-                $('#tWelcomeText').text('O foco da VGCJ é o planejamento de energia de armazenamento e a colaboração com especialistas em energia com o objetivo de cortar custos, economizar energia e reduzir as emissões de carbono. Além de nossa vasta experiência em equipamentos de baterias de armazenamento, também fornecemos equipamentos mecânicos e matérias-primas superiores e eficientes para fabricantes de baterias de íons de lítio e chumbo-ácido que podem reduzir a mão de obra e aumentar sua capacidade de produção. Estamos ansiosos para sermos parceiros de negócios!');
-            }
-        );
-
-        $('#flagTaiwan').on("click",
-            function () {
-                localStorage.setItem('lang', 'tw');
-                /* 
-                $('#mHome').text('開始');
-                $('#mAbout').text('後');
-                $('#mProducts').text('貨物');
-                $('#mServices').text('服務業');
-                $('#mContact').text('聯繫');
-                 */
-                /* $('#mProductLine').text('產品線');
-                $('#mAssemblyLine').text('裝配線');
-                $('#mPlateMaking').text('板材製造');
-                $('#mFinishLine').text('最後一行');
-                $('#mCompleteProject').text('完成專案');
-                $('#mUniqueEquipment').text('專用設備');
-                $('#mMaterialSupply').text('材料供應'); */
-
-                $('#tProductLine').text('產品線');
-                $('#tAssemblyLine').text('裝配線');
-                $('#tPlateMaking').text('板材製造');
-                $('#tFinishLine').text('最後一行');
-                $('#tCompleteProject').text('完成專案');
-                $('#tUniqueEquipment').text('專用設備');
-                $('#tMaterialSupply').text('材料供應');
-
-                $('#tWelcome').text('歡迎來到VGCJ');
-                $('#tWelcomeText').text('VGCJ的重點是存儲電源規劃以及與能源專家合作, 以降低成本，節約能源和減少碳排放。除了我們在蓄電池設備方面的豐富經驗外，我們還為鋰離子和鉛酸電池製造商提供優質高效的機械設備和原材料，可以減少工作力並提高生產能力。我們期待成為商業夥伴！');
-            }
-        );
-
-        if (!localStorage.getItem('lang') || localStorage.getItem('lang') == 'us') {
-            $("#flagUsa").trigger('click');
-        }
-        else if (localStorage.getItem('lang') == 'br') {
-            $("#flagBrazil").trigger('click');
-        }
-        else if (localStorage.getItem('lang') == 'tw') {
-            $("#flagTaiwan").trigger('click');
+        function funcaoExtraLocal() {
+            var language = JSON.parse(localStorage.getItem('language'));
+            document.getElementById("tProductLine").innerHTML = language.ProductLine.toUpperCase();
+            document.getElementById("tAssemblyLine").innerHTML = language.AssemblyLine.toUpperCase();
+            document.getElementById("tPlateMaking").innerHTML = language.PlateMaking.toUpperCase();
+            document.getElementById("tFinishLine").innerHTML = language.FinishLine.toUpperCase();
+            document.getElementById("tCompleteProject").innerHTML = language.CompleteProject.toUpperCase();
+            document.getElementById("tUniqueEquipment").innerHTML = language.UniqueEquipment.toUpperCase();
+            document.getElementById("tMaterialSupply").innerHTML = language.MaterialSupply.toUpperCase();
+            document.getElementById("tWelcome").innerHTML = language.Welcome;
+            document.getElementById("tWelcomeText").innerHTML = language.WelcomeText;
         }
 
-
-
+        HeaderFotter(funcaoExtraLocal);
     });
