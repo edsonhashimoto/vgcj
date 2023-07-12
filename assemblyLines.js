011781872701b045030ef7c6988402cd91e9f82f
edsonhashimoto <131090718+edsonhashimoto@users.noreply.github.com>
2023-07-11 21:48:25 -0300

    filtro

--- assemblyLines.js
+++ assemblyLines.js
@@ -8,15 +8,18 @@ $(document).ready(function () {
         const filterInput = document.getElementById('filter');
 
         let listModels = document.querySelectorAll('.product-model');
+        let listProductName = document.querySelectorAll('.product-name');
         let listFilter = [];
 
         listModels.forEach(model => {
             const textId = document.getElementById(model.getAttribute('id')).textContent.toLowerCase();
             const textGF = document.getElementById(model.getAttribute('id')+"GF").textContent.toLowerCase();
+            const groupName = document.getElementById(model.getAttribute('id')).getAttribute("class").split(" ").filter(x => x.startsWith('g'))[0];
 
             const obj = {
                 id : model.getAttribute('id'),
-                description: textId +" " + textGF
+                description: textId +" " + textGF,
+                groupName
             }
             listFilter.push(obj);
         });
@@ -27,12 +30,18 @@ $(document).ready(function () {
 
             const listModelsFilter = listFilter.filter(x => x.description.includes(filterValue));
 
+            listProductName.forEach(obj => {
+                document.getElementById(obj.getAttribute('id')).style.display = 'none';                
+            });
+
             listModels.forEach(obj => {
-                document.getElementById("r"+obj.id).style.display = 'none';
+                document.getElementById("r"+obj.id).style.display = 'none';                
             });
 
             listModelsFilter.forEach(obj => {
                 document.getElementById("r"+obj.id).style.display = 'flex';
+                document.getElementsByClassName(obj.groupName)[0].style.display = 'block';
+
             });
 
             //const listModelsFilter = listModels.filter(x => x.textContent.toLowerCase().includes(filterValue));
